0ce623fd7a620338ef046ca61ffbf3e955e99eaa
Oliver Barnum <oliverbarnum32@gmail.com>
2020-02-06 02:15:21 -0800

    Make main login

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import click
-from templateProcessor import ProcessTemplate
+import tcgClient
 
 @click.command()
-@click.option('--template', prompt='Template name', help='The name of the template used.')
-@click.option('--input', prompt='Input filepath', help='The filepath of the json file of the service.')
-@click.option('--output', prompt='Output path', help='The path of where the templated service will go.')
-def createTemplate(template, input, output):
-    """Produces a set of files based on a template and input file."""
-    ProcessTemplate(template, input, output)
+def createTemplate():
+    print(tcgClient.login())
 
 if __name__ == '__main__':
     createTemplate()
