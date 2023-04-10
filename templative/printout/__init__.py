1030ea321e3f213a565c57e91c868be92d072535
Oliver Barnum <oliverbarnum32@gmail.com>
2023-04-10 06:23:15 -0500

    Add front back printing

--- templative/printout/__init__.py
+++ templative/printout/__init__.py
@@ -7,7 +7,7 @@ from fpdf import FPDF
 
 from templative.gameUploader.instructionsLoader import getLastOutputFileDirectory
 
-componentDimensions = {
+componentDimensionsInches = {
     "PokerDeck": (2.5, 3.5),
     "MiniDeck": (1.75, 2.5),
     "MicroDeck": (1.25, 1.75),
@@ -36,88 +36,140 @@ async def front(input):
 
     await createPdfForPrinting(input, False)
 
-# @printout.command()
-# @click.option('-i', '--input', default=None, help='The directory of the produced game. Defaults to last produced directory.')
-# async def frontback(input):
-#     """Create a pdf for printing on the front and back"""
-#     if input is None:
-#         input = await getLastOutputFileDirectory()
+@printout.command()
+@click.option('-i', '--input', default=None, help='The directory of the produced game. Defaults to last produced directory.')
+async def frontback(input):
+    """Create a pdf for printing on the front and back"""
+    if input is None:
+        input = await getLastOutputFileDirectory()
 
-#     await createPdfForPrinting(input, True)
+    await createPdfForPrinting(input, True)
 
-async def createPdfForPrinting(producedDirectoryPath, frontBack):
+async def createPdfForPrinting(producedDirectoryPath, printBack):
     pdf = FPDF("P", "in", "Letter")
 
+    componentTypeFilepathAndQuantity = {}
     for directoryPath in next(walk(producedDirectoryPath))[1]:
-        createdPageImages = await convertComponentDirectoryToPageImages(producedDirectoryPath, directoryPath)
+        await loadFilepathsForComponent(componentTypeFilepathAndQuantity, producedDirectoryPath, directoryPath)
+    
+    for componentType in componentTypeFilepathAndQuantity.keys():
+        createdPageImages = await createPageImagesForComponentTypeImages(componentType, componentTypeFilepathAndQuantity[componentType], printBack)
         await addPageImagesToPdf(pdf, createdPageImages)
 
     pdf.output("./printout.pdf", "F")
 
-async def convertComponentDirectoryToPageImages(producedDirectoryPath, directoryPath):
+async def loadFilepathsForComponent(componentTypeFilepathAndQuantity, producedDirectoryPath, directoryPath):
     componentDirectoryPath = "%s/%s" % (producedDirectoryPath, directoryPath)
     componentInstructionsFilepath = path.join(componentDirectoryPath, "component.json")
     componentInstructionsFile = open(componentInstructionsFilepath, "r")
-
     componentInstructions = load(componentInstructionsFile)
-    createdPageImages = await createPageImagesFromPieceImages(componentInstructions)
+
+    await collectFilepathQuantitiesForComponent(componentTypeFilepathAndQuantity, componentInstructions)
     componentInstructionsFile.close()
-    # print(componentInstructions["name"], "created", len(createdPageImages), "pages")
-    return createdPageImages
+
+async def collectFilepathQuantitiesForComponent(componentTypeFilepathAndQuantity, componentInstructions):
+    if not componentInstructions["type"] in componentDimensionsInches:
+        print("Skipping unsupported %s named %s" %(componentInstructions["type"],componentInstructions["name"]))
+        return []
+    
+    if not componentInstructions["type"] in componentTypeFilepathAndQuantity:
+        componentTypeFilepathAndQuantity[componentInstructions["type"]] = []
+
+    for instruction in componentInstructions["frontInstructions"]:
+        frontBack = {
+            "filepath": instruction["filepath"],
+            "backFilepath": componentInstructions["backInstructions"]["filepath"],
+            "quantity":  int(instruction["quantity"]),
+
+        }
+        componentTypeFilepathAndQuantity[componentInstructions["type"]].append(frontBack)
 
 async def addPageImagesToPdf(pdf, pageImages):
     for image in pageImages:
         pdf.add_page()
         pdf.image(image,marginsInches,marginsInches,printoutPlayAreaInches[0],printoutPlayAreaInches[1])
 
-async def createPageImagesFromPieceImages(componentInstructions):
-    if not componentInstructions["type"] in componentDimensions:
-        print("Skipping unsupported %s named %s" %(componentInstructions["type"],componentInstructions["name"]))
+async def createPageImagesForComponentTypeImages(componentType, componentTypeImageList, printBack):
+    if not componentType in componentDimensionsInches:
+        print("Skipping unsupported %s" %(componentType))
         return []
     
-    componentSize = componentDimensions[componentInstructions["type"]]
-    pieceSizeInches = (componentSize[0] + pieceMarginInches, componentSize[1] + pieceMarginInches)
+    componentSizeInches = componentDimensionsInches[componentType]
+    pieceSizeInches = (componentSizeInches[0] + pieceMarginInches, componentSizeInches[1] + pieceMarginInches)
 
     columns = math.floor(printoutPlayAreaInches[0] / pieceSizeInches[0])
     rows = math.floor(printoutPlayAreaInches[1] / pieceSizeInches[1])
-    pageImages = await createBlankImagesForComponent(componentInstructions, columns, rows)
     
+    rotatedColumns = math.floor(printoutPlayAreaInches[0] / pieceSizeInches[1])
+    rotatedRows = math.floor(printoutPlayAreaInches[1] / pieceSizeInches[0])
+
+    isImageRotated = rotatedColumns*rotatedRows > columns*rows
+    if isImageRotated:
+        columns = rotatedColumns
+        rows = rotatedRows
+        pieceSizeInches = (pieceSizeInches[1], pieceSizeInches[0])
+        componentSizeInches = (componentSizeInches[1], componentSizeInches[0])
+
+    halfAreaPixels = (
+        int((printoutPlayAreaInches[0] - (pieceSizeInches[0] * columns)) / 2 * inchToPixelConversion), 
+        int((printoutPlayAreaInches[1] - (pieceSizeInches[1] * rows)) / 2 * inchToPixelConversion)
+    )
+
+    pageImages = await createBlankImagesForComponent(componentTypeImageList, columns, rows, printBack)
+    
+    resizedSizePixels = (
+        int(componentSizeInches[0] * inchToPixelConversion), 
+        int(componentSizeInches[1] * inchToPixelConversion)
+    )
+
     pieceIndex = 0
-    for f in range(len(componentInstructions["frontInstructions"])):
-        instruction = componentInstructions["frontInstructions"][f]
+    for f in range(len(componentTypeImageList)):
+        instruction = componentTypeImageList[f]
 
         pieceImage = Image.open(instruction["filepath"])
-        resizedSize = (
-            int(componentSize[0] * inchToPixelConversion), 
-            int(componentSize[1] * inchToPixelConversion)
-        )
-        print(resizedSize, "pixels")
-        pieceImage = pieceImage.resize(resizedSize)
+        backImage = Image.open(instruction["backFilepath"])
+
+        if isImageRotated:
+            pieceImage = pieceImage.rotate(-90, expand=True)
+            backImage = backImage.rotate(-90, expand=True)
+        
+        pieceImage = pieceImage.resize(resizedSizePixels)
+        backImage = backImage.resize(resizedSizePixels)
+
         for _ in range(int(instruction["quantity"])):
             pageIndex = math.floor(pieceIndex / (columns*rows))
-            pageImage = pageImages[pageIndex]
+            frontBackPageIndex = pageIndex * (2 if printBack else 1)
+            # print(pageIndex, len(pageImages))
+            pageImage = pageImages[frontBackPageIndex]
             pieceIndexOnPage = pieceIndex - (pageIndex * columns * rows)
             xIndex = math.floor(pieceIndexOnPage % columns)
             yIndex = math.floor(pieceIndexOnPage / columns)
             position = (
-                int(xIndex * pieceSizeInches[0] * inchToPixelConversion),
-                int(yIndex * pieceSizeInches[1] * inchToPixelConversion)
+                halfAreaPixels[0] + int(xIndex * pieceSizeInches[0] * inchToPixelConversion),
+                halfAreaPixels[1] + int(yIndex * pieceSizeInches[1] * inchToPixelConversion)
             )
             pageImage.paste(pieceImage, position)
+
+            if printBack:
+                pageImages[frontBackPageIndex+1].paste(backImage, position)
+
             pieceIndex+=1
+
+        pieceImage.close()
+        backImage.close()
 
     return pageImages
 
-async def createBlankImagesForComponent(componentInstructions, columns, rows):
+async def createBlankImagesForComponent(imageFilepaths, columns, rows, printBack):
     whiteColorRGB = (255,255,255)
 
     totalCount = 0
-    for instruction in componentInstructions["frontInstructions"]:
+    for instruction in imageFilepaths:
         totalCount += int(instruction["quantity"])
 
     itemsPerPage = columns * rows
-    totalPages = math.ceil(totalCount/itemsPerPage)
-    # print("Because a page fits", columns*rows, "we need", totalPages, "to accomidate", totalCount, componentInstructions["name"], "pieces")
+    totalPages = math.ceil(totalCount/itemsPerPage) * (2 if printBack else 1)
+    print("Because a page fits", columns*rows, "we need", totalPages, "(%s max)"%(columns*rows*totalPages), "to accomidate", totalCount * (2 if printBack else 1), "images", "frontback" if printBack else "")
     pageImages = []
     for _ in range(totalPages):
         imageSize = (int(printoutPlayAreaInches[0]*inchToPixelConversion), int(printoutPlayAreaInches[1]*inchToPixelConversion))
