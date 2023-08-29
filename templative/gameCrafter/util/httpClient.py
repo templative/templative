bbdab91f303331163f9ebfc0191a435071cbdd3f
Oliver Barnum <oliverbarnum32@gmail.com>
2023-08-29 15:00:14 -0500

    Print obnoxiously

--- templative/gameCrafter/util/httpClient.py
+++ templative/gameCrafter/util/httpClient.py
@@ -1,24 +1,34 @@
 import json
+from pprint import pprint
 
 async def post(gameCrafterSession, url, **kwargs):
     async with gameCrafterSession.httpSession.post(url, data=kwargs) as response:
-        return await handleResponse(url, response)
+        return await handleResponse(url, response, **kwargs)
 
 async def get(gameCrafterSession, url, **kwargs):
     async with gameCrafterSession.httpSession.get(url, params=kwargs) as response:
-        return await handleResponse(url, response)
+        return await handleResponse(url, response, **kwargs)
 
 async def delete(gameCrafterSession, url, **kwargs):
     async with gameCrafterSession.httpSession.delete(url, params=kwargs) as response:
-        return await handleResponse(url, response)
+        return await handleResponse(url, response, **kwargs)
 
-async def handleResponse(url, response):
+async def handleResponse(url, response, **kwargs):
     statusCode = str(response.status)
     responseText = await response.text()
     responseJson = json.loads(responseText)
     if not statusCode.startswith('2'):
-        print ('FAIL', response)
-        print ('FAIL', responseJson)
+        print('!!! Fail', url)
+        print(response)
+        print(responseJson)
+        print(kwargs)
+        print(**kwargs)
+        print(**kwargs.keys())
+        print(kwargs.keys())
+        pprint(kwargs)
+        pprint(**kwargs)
+        pprint(**kwargs.keys())
+        pprint(kwargs.keys())
         raise Exception('%s Returned %s.' % (url, statusCode))
 
     return responseJson['result']
