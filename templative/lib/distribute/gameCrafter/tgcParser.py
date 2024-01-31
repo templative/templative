c2f62e2cf8307fce1137cc9f201e026800ea8b59
Oliver Barnum <oliverbarnum32@gmail.com>
2024-01-31 12:31:58 -0600

    Update stock part parser

--- templative/lib/distribute/gameCrafter/tgcParser.py
+++ templative/lib/distribute/gameCrafter/tgcParser.py
@@ -1,6 +1,7 @@
 import json
 from os import path
 from templative.lib.distribute.gameCrafter.util import httpOperations
+import time
 
 INCH_TO_MILLIMETERS = 25.4
 async def parseCustomStuff (gameCrafterSession):
@@ -37,6 +38,12 @@ async def parseCustomStuff (gameCrafterSession):
     with open('./customComponents.json', 'w') as f:
         json.dump(componentInfo, f, indent=4)
 
+def tagListHasColor(tagList, possibleColor): 
+    for tag in tagList:
+        if tag.lower() == possibleColor.lower():
+            return True
+    return False
+
 async def parseStockStuff(gameCrafterSession):
     pageNumber = 1
     stockInfo = {}
@@ -45,12 +52,27 @@ async def parseStockStuff(gameCrafterSession):
         print("Reading page %s of %s." % (stockPartInfoPage["paging"]["page_number"], stockPartInfoPage["paging"]["total_pages"]))
         
         for component in stockPartInfoPage["items"]:
-            stockInfo[component["name"]] = {
-                "GameCrafterId": component["sku_id"],
-                "GameCrafterSkuId": component["sku_id"]
+            varname = "".join(component["name"].replace(",", "").split(" "))
+            tags = []
+            if "keywords" in component and component["keywords"] != None:
+                tags = component["keywords"].split(", ")
+
+            for index, item in enumerate(tags):
+                tags[index] = item.lower()
+            if "color" in component and component["color"] != None:
+                color = component["color"].lower()
+                if not tagListHasColor(tags, color):
+                    tags.append(color)
+            stockInfo[varname] = {
+                "DisplayName": component["name"],
+                "Description": component["description"],
+                "GameCrafterId": component["id"],
+                "GameCrafterSkuId": component["sku_id"],
+                "Tags": tags
             }
         if int(stockPartInfoPage["paging"]["page_number"]) >= int(stockPartInfoPage["paging"]["total_pages"]):
             break
         pageNumber = pageNumber + 1
+        time.sleep(1/4)
     with open('./stockComponents.json', 'w') as f:
         json.dump(stockInfo, f, indent=4)
